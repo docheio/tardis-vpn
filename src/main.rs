9f1604901fa67d6e76dbe6560a4b0fe497704f05
ES-Yukun <yukun@doche.io>
2023-08-17 16:18:09 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,12 @@ use modules::server::server;
 
 #[tokio::main]
 async fn main() {
-    let mode = env::args().nth(2).expect("Unable to select mode");
-    if (&mode).eq("peer") {
+    let mode = env::args().nth(1).expect("Unable to select mode");
+    if mode.eq("peer") {
         peer().await;
-    } else if (&mode).eq("server") {
+    } else if mode.eq("server") {
         server().await;
-    } else if (&mode).eq("client") {
+    } else if mode.eq("client") {
         client().await;
     } else {
         eprintln!("Unable to select mode");
