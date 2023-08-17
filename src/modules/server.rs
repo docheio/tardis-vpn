5cddb64a06777fba35bb427240ac613fbb564083
ES-Yukun <yukun@doche.io>
2023-08-18 03:29:57 +0900

    update

--- src/modules/server.rs
+++ src/modules/server.rs
@@ -15,11 +15,11 @@ use std::net::SocketAddr;
 use std::process::Command;
 use std::{env, process};
 
-use futures::{Future, Stream};
+// use futures::{Future, Stream};
 use tokio_core::net::{UdpCodec, UdpSocket};
 use tokio_core::reactor::Core;
 
-use tun_tap::asynclib::Async;
+// use tun_tap::asynclib::Async;
 use tun_tap::{Iface, Mode};
 
 struct VecCodec(SocketAddr);
@@ -60,12 +60,10 @@ pub async fn server() {
         eprintln!("Unable to open socket: {}", err);
         process::exit(1);
     });
-    println!("ok0");
     let mut buf = vec![0; 10];
-    println!("ok1");
-    let (_, rem_address) = socket.recv_from(&mut buf).unwrap();
+    let _ = socket.recv_from(&mut buf);
     println!("ok2");
-    let (sender, receiver) = socket.framed(VecCodec(rem_address)).split();
+    // let (sender, receiver) = socket.framed(VecCodec(rem_address)).split();
 
     // Create interface
     let name = &env::args().nth(3).expect("Unable to read Interface name");
@@ -82,8 +80,8 @@ pub async fn server() {
     cmd("ip", &["link", "set", "up", "dev", tap.name()]);
 
     // Handshake
-    let (sink, stream) = Async::new(tap, &core.handle()).unwrap().split();
-    let reader = stream.forward(sender);
-    let writer = receiver.forward(sink);
-    core.run(reader.join(writer)).unwrap();
+    // let (sink, stream) = Async::new(tap, &core.handle()).unwrap().split();
+    // let reader = stream.forward(sender);
+    // let writer = receiver.forward(sink);
+    // core.run(reader.join(writer)).unwrap();
 }
