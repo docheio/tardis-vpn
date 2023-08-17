7bc9603450bd56b46a3e4404ddf4b4997579175a
ES-Yukun <yukun@doche.io>
2023-08-18 04:28:06 +0900

    update

--- src/modules/client.rs
+++ src/modules/client.rs
@@ -10,31 +10,13 @@
 /*                                                                                                            */
 /* ********************************************************************************************************** */
 
-use std::io::Result;
 use std::net::SocketAddr;
 use std::process::Command;
 use std::{env, process};
 
-use futures::{Future, Stream};
-use tokio_core::net::{UdpCodec, UdpSocket};
-use tokio_core::reactor::Core;
+use tokio::net::UdpSocket;
 
-use tun_tap::asynclib::Async;
 use tun_tap::{Iface, Mode};
-
-struct VecCodec(SocketAddr);
-
-impl UdpCodec for VecCodec {
-    type In = Vec<u8>;
-    type Out = Vec<u8>;
-    fn decode(&mut self, _src: &SocketAddr, buf: &[u8]) -> Result<Self::In> {
-        Ok(buf.to_owned())
-    }
-    fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> SocketAddr {
-        buf.extend(&msg);
-        self.0
-    }
-}
 
 fn cmd(cmd: &str, args: &[&str]) {
     let ecode = Command::new(cmd)
@@ -47,42 +29,49 @@ fn cmd(cmd: &str, args: &[&str]) {
 }
 
 pub async fn client() {
-    let mut core = Core::new().unwrap();
+    // Read Remote IP from args
+    let rem_address = env::args()
+        .nth(2)
+        .unwrap()
+        .parse::<SocketAddr>()
+        .unwrap_or_else(|err| {
+            eprintln!("Unable to recognize remote ip: {}", err);
+            process::exit(1);
+        });
 
-    // Read Local & Remote IP from args
-    let loc_address = env::args().nth(2).unwrap().parse().unwrap_or_else(|err| {
-        eprintln!("Unable to recognize listen ip: {}", err);
-        process::exit(1);
-    });
-    let rem_address = env::args().nth(3).unwrap().parse().unwrap_or_else(|err| {
-        eprintln!("Unable to recognize remote ip: {}", err);
+    // Allocate an available ip:port
+    let loc_address = "0.0.0.0:0".parse::<SocketAddr>().unwrap_or_else(|err| {
+        eprintln!("Unable to bind udp socket: {}", err);
         process::exit(1);
     });
 
     // Create socket
-    let socket = UdpSocket::bind(&loc_address, &core.handle()).unwrap();
-    socket.connect(&rem_address).unwrap();
-    let mut buf = vec![0; 10];
-    socket.send(&mut buf).unwrap();
-    let (sender, receiver) = socket.framed(VecCodec(rem_address)).split();
+    let socket = UdpSocket::bind(&loc_address).await.unwrap();
 
     // Create interface
-    let name = &env::args().nth(4).expect("Unable to read Interface name");
-    let tap = Iface::new(&name, Mode::Tap).unwrap_or_else(|err| {
+    let name = &env::args().nth(3).expect("Unable to read Interface name");
+    let iface = Iface::new(&name, Mode::Tap).unwrap_or_else(|err| {
         eprintln!("Failed to configure the interface name: {}", err);
         process::exit(1);
     });
 
     // Configure the „local“ (kernel) endpoint.
     let ip = &env::args()
-        .nth(5)
+        .nth(4)
         .expect("Unable to recognize remote interface IP");
-    cmd("ip", &["addr", "add", "dev", tap.name(), &ip]);
-    cmd("ip", &["link", "set", "up", "dev", tap.name()]);
+    cmd("ip", &["addr", "add", "dev", iface.name(), &ip]);
+    cmd("ip", &["link", "set", "up", "dev", iface.name()]);
 
     // Handshake
-    let (sink, stream) = Async::new(tap, &core.handle()).unwrap().split();
-    let reader = stream.forward(sender);
-    let writer = receiver.forward(sink);
-    core.run(reader.join(writer)).unwrap();
+    let mut buf = [0; 1504];
+    socket.connect(&rem_address).await.unwrap_or_else(|err| {
+        eprintln!("Unable to connect to server: {}", err);
+        process::exit(1);
+    });
+    loop {
+        let len = iface.recv(&mut buf).unwrap();
+        socket.send(&buf[..len]).await.unwrap();
+        let len = socket.recv(&mut buf).await.unwrap();
+        iface.send(&buf[..len]).unwrap();
+    }
 }
--- src/modules/server.rs
+++ src/modules/server.rs
@@ -10,31 +10,13 @@
 /*                                                                                                            */
 /* ********************************************************************************************************** */
 
-use std::io::Result;
 use std::net::SocketAddr;
 use std::process::Command;
 use std::{env, process};
 
-// use futures::{Future, Stream};
-use tokio_core::net::{UdpCodec, UdpSocket};
-use tokio_core::reactor::Core;
+use tokio::net::UdpSocket;
 
-// use tun_tap::asynclib::Async;
 use tun_tap::{Iface, Mode};
-
-struct VecCodec(SocketAddr);
-
-impl UdpCodec for VecCodec {
-    type In = Vec<u8>;
-    type Out = Vec<u8>;
-    fn decode(&mut self, _src: &SocketAddr, buf: &[u8]) -> Result<Self::In> {
-        Ok(buf.to_owned())
-    }
-    fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> SocketAddr {
-        buf.extend(&msg);
-        self.0
-    }
-}
 
 fn cmd(cmd: &str, args: &[&str]) {
     let ecode = Command::new(cmd)
@@ -47,27 +29,25 @@ fn cmd(cmd: &str, args: &[&str]) {
 }
 
 pub async fn server() {
-    let mut core = Core::new().unwrap();
-
     // Read Local & Remote IP from args
-    let loc_address = env::args().nth(2).unwrap().parse().unwrap_or_else(|err| {
-        eprintln!("Unable to recognize listen ip: {}", err);
-        process::exit(1);
-    });
+    let loc_address = env::args()
+        .nth(2)
+        .unwrap()
+        .parse::<SocketAddr>()
+        .unwrap_or_else(|err| {
+            eprintln!("Unable to recognize listen ip: {}", err);
+            process::exit(1);
+        });
 
     // Create socket
-    let socket = UdpSocket::bind(&loc_address, &core.handle()).unwrap_or_else(|err| {
-        eprintln!("Unable to open socket: {}", err);
+    let socket = UdpSocket::bind(&loc_address).await.unwrap_or_else(|err| {
+        eprintln!("Unable to bind udp socket: {}", err);
         process::exit(1);
     });
-    let mut buf = vec![0; 10];
-    let _ = socket.recv_from(&mut buf);
-    println!("ok2");
-    // let (sender, receiver) = socket.framed(VecCodec(rem_address)).split();
 
     // Create interface
     let name = &env::args().nth(3).expect("Unable to read Interface name");
-    let tap = Iface::new(&name, Mode::Tap).unwrap_or_else(|err| {
+    let iface = Iface::new(&name, Mode::Tap).unwrap_or_else(|err| {
         eprintln!("Failed to configure the interface name: {}", err);
         process::exit(1);
     });
@@ -76,12 +56,15 @@ pub async fn server() {
     let ip = &env::args()
         .nth(4)
         .expect("Unable to recognize remote interface IP");
-    cmd("ip", &["addr", "add", "dev", tap.name(), &ip]);
-    cmd("ip", &["link", "set", "up", "dev", tap.name()]);
+    cmd("ip", &["addr", "add", "dev", iface.name(), &ip]);
+    cmd("ip", &["link", "set", "up", "dev", iface.name()]);
 
     // Handshake
-    // let (sink, stream) = Async::new(tap, &core.handle()).unwrap().split();
-    // let reader = stream.forward(sender);
-    // let writer = receiver.forward(sink);
-    // core.run(reader.join(writer)).unwrap();
+    let mut buf = [0; 1504];
+    loop {
+        let (len, addr) = socket.recv_from(&mut buf).await.unwrap();
+        iface.send(&buf[..len]).unwrap();
+        let len = iface.recv(&mut buf).unwrap();
+        socket.send_to(&buf[..len], addr).await.unwrap();
+    }
 }
