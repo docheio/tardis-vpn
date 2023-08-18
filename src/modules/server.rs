1ac141400ff4dfaaf6c149bbaefb3f87babadde0
ES-Yukun <yukun@doche.io>
2023-08-18 20:28:33 +0900

    update

--- src/modules/server.rs
+++ src/modules/server.rs
@@ -62,54 +62,34 @@ pub async fn server() {
     let iface = Arc::new(iface);
     let iface_writer = Arc::clone(&iface);
     let iface_reader = Arc::clone(&iface);
-    let socket_keep = socket.clone();
     let socket_send = socket.clone();
     let socket_recv = socket.clone();
 
-    {
-        let mut buf = vec![0; 1500];
-        let (_, addr) = socket.recv_from(&mut buf).unwrap();
+    let mut buf = vec![0; 1500];
+    let (_, addr) = socket.recv_from(&mut buf).unwrap();
 
-        let keeper = thread::spawn(move || {
-            println!("k loaded");
-            loop {
-                match socket_keep.send_to(&vec![], addr) {
-                    Ok(_) => {}
-                    Err(_) => {
-                        break;
-                    }
-                };
+    let writer = thread::spawn(move || {
+        println!("w loaded");
+        loop {
+            let mut buf = vec![0; 1500];
+            let len = socket_recv.recv(&mut buf).unwrap();
+            println!("recv: {:?}", len);
+            iface_writer.send(&buf[..len]).unwrap();
+        }
+    });
+    
+    let reader = thread::spawn(move || {
+        println!("r loaded");
+        loop {
+            let mut buf = vec![0; 1504];
+            let len = iface_reader.recv(&mut buf).unwrap();
+            println!("if recv");
+            if len > 0 {
+                socket_send.send_to(&buf[..len], &addr).unwrap();
+                println!("send: {:?}", len);
             }
-        });
-        let writer = thread::spawn(move || {
-            println!("w loaded");
-            loop {
-                if keeper.is_finished() {
-                    break;
-                }
-                let mut buf = vec![0; 1500];
-                let len = socket_recv.recv(&mut buf).unwrap();
-                println!("recv: {:?}", len);
-                iface_writer.send(&buf[..len]).unwrap();
-            }
-        });
-        let _ = thread::spawn(move || {
-            println!("r loaded");
-            loop {
-                let mut buf = vec![0; 1504];
-                let len = iface_reader.recv(&mut buf).unwrap();
-                println!("if recv");
-                if len > 0 {
-                    match socket_send.send_to(&buf[..len], &addr) {
-                        Ok(_) => {}
-                        Err(_) => {
-                            break;
-                        }
-                    };
-                    println!("send: {:?}", len);
-                }
-            }
-        });
-        writer.join().unwrap();
-    }
+        }
+    });
+    writer.join().unwrap();
+    reader.join().unwrap();
 }
